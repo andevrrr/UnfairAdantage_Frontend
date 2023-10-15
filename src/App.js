50503247fcd962ac0fe5d1846dcd48d540825ea6
Anton <kucherenko-anton1@mail.ru>
2023-10-15 19:14:30 +0300

    week-slider implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import WeekSlider from './WeekSelector';
 
 function App() {
   return (
-    <div className="App">
-      Hi
-    </div>
+    <>
+    <WeekSlider week={1} initialValues={[0, 6]} />
+    <WeekSlider week={2} initialValues={[1, 5]} />
+    <WeekSlider week={3} initialValues={[1, 5]} />
+    <WeekSlider week={4} initialValues={[1, 5]} />
+    <WeekSlider week={5} initialValues={[1, 5]} />
+    <WeekSlider week={6} initialValues={[1, 5]} />
+    <WeekSlider week={7} initialValues={[1, 5]} />
+    </>
   );
 }
 
---
+++ src/WeekSelector.js
@@ -0,0 +1,55 @@
+// WeekSlider.js
+
+import React, { useState } from 'react';
+import ReactSlider from 'react-slider';
+import './styles.css'; 
+
+const WeekSlider = ({ week, initialValues }) => {
+  const [values, setValues] = useState(initialValues);
+
+  const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+  const handleValuesChange = (newValues) => {
+    setValues(newValues);
+  };
+
+  const isDayInInterval = (dayIndex) => {
+    return dayIndex >= values[0] && dayIndex <= values[1];
+  };
+
+  return (
+    <div className='container'>
+      <div className="week-slider-container">
+        <h2>Week {week}</h2>
+        <ReactSlider
+          className="horizontal-slider"
+          thumbClassName="example-thumb"
+          trackClassName="example-track"
+          defaultValue={values}
+          ariaValuetext={(state) => `Thumb value ${daysOfWeek[state.valueNow]}`}
+          pearling
+          minDistance={1}
+          max={6}
+          step={1}
+          onChange={handleValuesChange}
+        >
+        </ReactSlider>
+        <div className="days-of-week">
+          {daysOfWeek.map((day, index) => (
+            <div
+              key={index}
+              className={`day-label ${isDayInInterval(index) ? 'selected' : ''}`}
+            >
+              <div className='days'> {day} </div>
+            </div>
+          ))}
+        </div>
+        <div className="circle-left" style={{ left: `calc(${values[0] * (100 / 6)}% - 7.5px)` }} />
+        <div className="circle-right" style={{ left: `calc(${values[1] * (100 / 6)}% - 7.5px)` }} />
+      </div>
+    </div>
+  );
+};
+
+export default WeekSlider;
+
